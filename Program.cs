5e3373e24f430f687734541f28371cdd215b77b0
Kurt Claeys <kurt.claeys@outlook.com>
2020-03-11 00:01:22 +0100

    test

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sbmcourse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! abc");
+        }
+    }
+}
