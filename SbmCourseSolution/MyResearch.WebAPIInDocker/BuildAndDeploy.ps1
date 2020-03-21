556e2dbf1ca27df9f7b882f5f64c41eaad4304c6
Kurt Claeys <kurt@nuboid.com>
2020-03-21 20:49:44 +0100

    GenerateBarcode

--- SbmCourseSolution/MyResearch.WebAPIInDocker/BuildAndDeploy.ps1
+++ SbmCourseSolution/MyResearch.WebAPIInDocker/BuildAndDeploy.ps1
@@ -13,6 +13,7 @@ else
 }
 
 dotnet publish  -c Release -o $buildpath
+
 Copy-Item -Path DockerfileToDeploy -Destination $buildpath\Dockerfile
 
 docker stop $runningcontainername
@@ -20,6 +21,9 @@ docker rm $runningcontainername
 docker rmi $imagename
 
 docker build -t $imagename -f $buildpath\Dockerfile $buildpath
+
+Remove-Item -LiteralPath $buildpath -Recurse
+
 docker container run -it  -p 5000:5000 --name $runningcontainername $imagename
 
 
---
+++ SbmCourseSolution/MySoftwareCompany.Services.GenerateBarcode/Controllers/BarCodeGeneratorController.cs
@@ -0,0 +1,28 @@
+﻿using Barcoder.Qr;
+using Barcoder.Renderer.Image;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace MySoftwareCompany.Services.GenerateBarcode.Controllers
+{
+    [ApiController]
+    public class BarCodeGeneratorController : Controller
+    {
+        [HttpGet("api/ping")]
+        public string GetPing()
+        {
+            return "pong";
+        }
+
+        [HttpGet("api/barcode/{data}")]
+        public IActionResult GetBarcode(string data)
+        {
+            var barcode = QrEncoder.Encode(data, ErrorCorrectionLevel.Q, Encoding.Auto);
+            var renderer = new ImageRenderer();
+            var stream = new MemoryStream();
+            renderer.Render(barcode, stream);
+
+            return File(stream.ToArray(), "image/png");
+        }
+    }
+}
