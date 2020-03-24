fde8def83a80db4da97a5e44b29d86d73b25d5cd
Kurt Claeys <kurt@nuboid.com>
2020-03-24 15:06:35 +0100

    MongoDB Examples

--- SbmCourseSolution/MyResearch.MongoDB/Program.cs
+++ SbmCourseSolution/MyResearch.MongoDB/Program.cs
@@ -15,25 +15,77 @@ namespace MyResearch.MongoDB
         {
             Console.WriteLine("MongoDB");
 
-            //var dbClient = new MongoClient("mongodb://localhost:27017");
 
-            //var dbList = dbClient.ListDatabases().ToList();
+            //ListDatabases();
+            //RunStats();
+            DeleteAllDocumentsInCollection();
+            InsertDocumentsWithBookService();
+            FindDocument();
+            
+            Console.WriteLine("Done");
 
-            //Console.WriteLine("The list of databases on this server is: ");
-            //foreach (var db in dbList)
-            //{
-            //    Console.WriteLine(db);
-            //}
+            Console.ReadKey();
+        }
 
+        private static void ListDatabases()
+        {
+            var dbClient = new MongoClient("mongodb://localhost:27017");
+
+            var dbList = dbClient.ListDatabases().ToList();
+
+            Console.WriteLine("The list of databases on this server is: ");
+            foreach (var db in dbList)
+            {
+                Console.WriteLine(db);
+            }
+        }
+        private static void RunStats()
+        {
+            var dbClient = new MongoClient("mongodb://localhost:27017");
+
+            IMongoDatabase db = dbClient.GetDatabase("nuboidtest");
+
+            var command = new BsonDocument { { "dbstats", 1 } };
+            var result = db.RunCommand<BsonDocument>(command);
+            Console.WriteLine(result.ToJson());
+        }
+
+        private static void DeleteAllDocumentsInCollection()
+        {
+            var dbClient = new MongoClient("mongodb://localhost:27017");
+
+            IMongoDatabase db = dbClient.GetDatabase("nuboidtest");
+
+            var collection = db.GetCollection<Book>("MyCollection");
+            collection.DeleteMany(c => true);
+        }
+        private static void FindDocument()
+        {
+            var dbClient = new MongoClient("mongodb://localhost:27017");
+
+            IMongoDatabase db = dbClient.GetDatabase("nuboidtest");
+            var collection = db.GetCollection<BsonDocument>("MyCollection");
+
+            var filter = Builders<BsonDocument>.Filter.Eq("Category", "History");
+
+            foreach (var doc in collection.Find(filter).ToList())
+            {
+                Console.WriteLine(doc.ToString());
+            }
+                
+            
+        }
+        private static void InsertDocumentsWithBookService()
+        {
             var bookService = new BookService("mongodb://localhost:27017", "nuboidtest", "MyCollection");
 
             bookService.Create(new Book()
             {
                 Id = ObjectId.GenerateNewId().ToString(),
-                Author ="Jef Nys",
-                BookName= "Jommeke 57 - Het geheim van Macu Ancapa",
-                Category ="Strips",
-                Price  = 3.14d 
+                Author = "Jef Nys",
+                BookName = "Jommeke 57 - Het geheim van Macu Ancapa",
+                Category = "Strips",
+                Price = 3.14d
             });
 
             bookService.Create(new Book()
@@ -54,11 +106,14 @@ namespace MyResearch.MongoDB
                 Price = 3.14d
             });
 
-            
-
-            Console.WriteLine("Done");
-
-            Console.ReadKey();
+            bookService.Create(new Book()
+            {
+                Id = ObjectId.GenerateNewId().ToString(),
+                Author = "Yuval Noah Harari",
+                BookName = "Sapiens - A Brief History of Humankind",
+                Category = "History",
+                Price = 3.14d
+            });
         }
     }
 }
