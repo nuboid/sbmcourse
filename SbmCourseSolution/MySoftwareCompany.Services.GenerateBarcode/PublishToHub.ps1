84e10352b057714e27cd141f26a0ea4fad725ac7
Kurt Claeys <kurt@nuboid.com>
2020-03-25 20:34:57 +0100

    testcommit

--- SbmCourseSolution/MySoftwareCompany.Services.GenerateBarcode/PublishToHub.ps1
+++ SbmCourseSolution/MySoftwareCompany.Services.GenerateBarcode/PublishToHub.ps1
@@ -1,8 +1,7 @@
 
 #remember to set new version !
 
-docker tag generatebarcodeimage nuboid/generatebarcodeimage:1.0
+docker tag generatebarcodeimage nuboid/generatebarcodeimage:1.1
 docker login
-docker push nuboid/generatebarcodeimage:1.0
-
+docker push nuboid/generatebarcodeimage:1.1
 
